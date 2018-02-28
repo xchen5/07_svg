d2c0385591a5cc1b8f70057f23914ae6c47c54fd
xchen5 <xchen5@stuy.edu>
2018-02-28 18:03:00 -0500

    finished

--- svg.js
+++ svg.js
@@ -1,7 +1,25 @@
 var pic = document.getElementById("a");
+var clear = document.getElementById("clear");
+var prevX = -1;
+var prevY = -1;
+
+
 var createCircle = function(e){
     var x = e.offsetX;
     var y = e.offsetY;
+    var c2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    if(prevX === -1){
+      prevX = e.offsetX;
+      prevY = e.offsetY;
+    }
+    c2.setAttribute("x1", prevX);
+    c2.setAttribute("y1", prevY);
+    c2.setAttribute("x2", x);
+    c2.setAttribute("y2", y);
+    c2.setAttribute("style", "stroke: black;")
+    prevX = e.offsetX;
+    prevY = e.offsetY;
+    pic.appendChild(c2);
     var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c1.setAttribute("cx", x);
     c1.setAttribute("cy", y);
@@ -9,4 +27,9 @@ var createCircle = function(e){
     pic.appendChild(c1);
 }
 
+var toClear = function(e){
+  pic.innerHTML = '';
+}
+
 pic.addEventListener("click", createCircle);
+clear.addEventListener("click", toClear);
